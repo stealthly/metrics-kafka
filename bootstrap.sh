db6c2af90662d5495077f216071e78135cf41e5c
edgefox <ivanlyutov@gmail.com>
2014-06-14 21:53:57 +0300

    Simplified setup process.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
-
-apt-get -y update
-apt-get install -y software-properties-common python-software-properties
-add-apt-repository -y ppa:webupd8team/java
-apt-get -y update
-/bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-apt-get -y install oracle-java7-installer oracle-java7-set-default
-
 export START_KAFKA_SCRIPT=https://raw2.github.com/stealthly/docker-kafka/master/start-broker.sh
 curl -Ls $START_KAFKA_SCRIPT | bash /dev/stdin 1 9092 localhost
 
 export START_RIEMANN_SCRIPT=https://raw2.github.com/stealthly/docker-riemann/master/start-riemann.sh
 curl -Ls $START_RIEMANN_SCRIPT | bash /dev/stdin localhost
+
+sudo docker run --link broker1:localhost -d -v $(pwd)/psutil/src/main/python:/psutil stealthly/docker-python python /psutil/psutil_producer.py --topic psutil-kafka-topic --url localhost:9092
--- metrics-client/src/main/scala/ly/stealth/psutil/PsutilKafka.scala
+++ metrics-client/src/main/scala/ly/stealth/psutil/PsutilKafka.scala
@@ -4,15 +4,12 @@ import java.util.UUID
 import ly.stealth.kafka.riemann.PsutilMetricsConsumer
 
 object PsutilKafka extends App {
-  val command = "jar xf psutil.jar psutil_producer.py && /usr/bin/python --topic %s --async %s --sendInBatch %s url %s rm psutil_producer.py"
-  val process: Process = Runtime.getRuntime.exec(command.format(args(0), args(1), args(2), args(3)))
-  val consumer = new PsutilMetricsConsumer(args(4),
-                                           args(5).toInt,
-                                           "basic description",
-                                           "kafka",
-                                           args(0),
-                                           UUID.randomUUID().toString,
-                                           args(6),
-                                           30000)
+  val consumer = new PsutilMetricsConsumer(riemannHost = args(1),
+                                           riemannPort = args(2).toInt,
+                                           descriptionText = "basic description",
+                                           tagList = "kafka",
+                                           groupId = UUID.randomUUID().toString,
+                                           topic = args(0),
+                                           zookeeperConnect = args(3))
   consumer.transfer()
 }
