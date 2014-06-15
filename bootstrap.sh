6eb52ef0268d44b6a25c61e93a8bba8363afb9de
edgefox <ivanlyutov@gmail.com>
2014-06-15 17:39:17 +0300

    Added psutil shutdown

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,4 +16,4 @@ curl -Ls $START_KAFKA_SCRIPT | bash /dev/stdin 1 9092 localhost
 export START_RIEMANN_SCRIPT=https://raw2.github.com/stealthly/docker-riemann/master/start-riemann.sh
 curl -Ls $START_RIEMANN_SCRIPT | bash /dev/stdin localhost
 
-sudo docker run --link broker1:localhost -d -v $(pwd)/psutil/src/main/python:/psutil stealthly/docker-python python /psutil/psutil_producer.py --topic psutil-kafka-topic --url localhost:9092
+sudo docker run --name psutil --link broker1:localhost -d -v $(pwd)/psutil/src/main/python:/psutil stealthly/docker-python python /psutil/psutil_producer.py --topic psutil-kafka-topic --url localhost:9092
--- shutdown.sh
+++ shutdown.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+docker kill psutil
 docker kill riemann
 docker kill zkserver
 docker kill broker1
 
+docker rm psutil
 docker rm riemann
-docker rm zkserver
 docker rm broker1
+docker rm zkserver
