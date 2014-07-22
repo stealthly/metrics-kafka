afe51a7853266f7a2d33a50596a043219c130cb6
Ivan Lyutov <ivanlyutov@gmail.com>
2014-07-22 18:24:03 +0300

    removed java install condition

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-JAVA_PATH=$(type -p java)
-if [ -z "$JAVA_PATH" ]; then
-    sudo apt-get -y update
-    sudo apt-get install -y software-properties-common python-software-properties
-    sudo add-apt-repository -y ppa:webupd8team/java
-    sudo apt-get -y update
-    sudo /bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
-    sudo apt-get -y install oracle-java7-installer oracle-java7-set-default
-fi
+sudo apt-get -y update
+sudo apt-get install -y software-properties-common python-software-properties
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get -y update
+sudo /bin/echo debconf shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+sudo apt-get -y install oracle-java7-installer oracle-java7-set-default
 
 export START_KAFKA_SCRIPT=https://raw2.github.com/stealthly/docker-kafka/master/start-broker.sh
 curl -Ls $START_KAFKA_SCRIPT | bash /dev/stdin 1 9092 localhost
