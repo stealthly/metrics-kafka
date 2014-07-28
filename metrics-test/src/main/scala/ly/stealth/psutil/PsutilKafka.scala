ba4bed4ad8e7bc194f8aa83f9bf016d785a264ac
Ivan Lyutov <ivanlyutov@gmail.com>
2014-07-28 19:36:34 +0300

    Updated metrics test to run all metric consumers.

--- metrics-test/src/main/scala/ly/stealth/psutil/PsutilKafka.scala
+++ metrics-test/src/main/scala/ly/stealth/psutil/PsutilKafka.scala
@@ -2,13 +2,29 @@ package ly.stealth.psutil
 
 import java.util.UUID
 import java.util.concurrent.TimeUnit
+import ly.stealth.kafka.metrics.KafkaGraphiteConsumer
 import ly.stealth.kafka.metrics.statsd.KafkaPsutilStatsDConsumer
+import ly.stealth.kafka.riemann.PsutilMetricsConsumer
 
 object PsutilKafka extends App {
-  val psutilStatsdConsumer = new KafkaPsutilStatsDConsumer(null, args(1), 8125, false, 1024, args(0),
-    UUID.randomUUID().toString, args(3), 30000, true)
+  val riemann = new Thread(new Runnable() {
+    def run(): Unit = {
+      val consumer = new PsutilMetricsConsumer(riemannHost = args(1),
+        riemannPort = 5555,
+        descriptionText = "basic description",
+        tagList = "kafka",
+        groupId = UUID.randomUUID().toString,
+        topic = args(0),
+        zookeeperConnect = args(3))
+      consumer.transfer()
+    }
+  })
 
-  psutilStatsdConsumer.start()
+  new KafkaPsutilStatsDConsumer(null, args(1), 8125, false, 1024, args(0),UUID.randomUUID().toString, args(3), 30000, true).start()
+  new KafkaGraphiteConsumer(args(1), 2003, args(3), "kafka-metrics-topic", UUID.randomUUID().toString, 30000, true).start()
+  riemann.start()
+
   TimeUnit.SECONDS.sleep(30)
-  psutilStatsdConsumer.stop()
+
+  System.exit(0)
 }
